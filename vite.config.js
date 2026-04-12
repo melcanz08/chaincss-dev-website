c1ed77b39aaa2e86b7d82ccaa312b71f35120fc1
Rommel Caneos <rec0608m@gmail.com>
2026-04-12 11:09:00 +0800

    Fix Vue peer dependency issue in chaincss

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['chaincssv2', 'caniuse-db']
+    exclude: ['vue']
   },
   ssr: {
-    noExternal: ['chaincssv2']
-  },
-  build: {
-    rollupOptions: {
-      external: ['chaincssv2', 'caniuse-db']
-    }
-  },
-  server: {
-    watch: {
-      ignored: ['**/*.chain.js']  // Ignore .chain.js files
-    }
+    noExternal: ['chaincss']
   }
-});
+})
