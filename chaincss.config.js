60878c757b94fc47214008d23894ad5bf2366d1c
Rommel Caneos <rec0608m@gmail.com>
2026-06-26 21:42:00 +0800

    update to ChainCSS v2.8.3 with Vite plugin, keep existing .class.js files for compat

---
+++ chaincss.config.js
@@ -0,0 +1,5 @@
+export default { 
+  atomic: { enabled: true, naming: 'readable' },
+  verbose: true,
+  prefixer: { enabled: true, verbose: true }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import chaincss from 'chaincss/plugin/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    chaincss({
+      verbose: true,
+      minify: true,
+    })
+  ],
   optimizeDeps: {
     exclude: ['vue']
-  },
-  ssr: {
-    noExternal: ['chaincss']
   }
 })
